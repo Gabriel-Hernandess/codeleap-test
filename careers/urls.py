561c24bdb8f12e38963617314c31e357e774a906
Gabriel Hernandes <gabriel.hernandess01@gmail.com>
2025-09-03 17:17:29 -0300

    Adicionado página de Home para apresentação do projeto junto de partials para includes no template do app homepage.

--- careers/urls.py
+++ careers/urls.py
@@ -4,6 +4,4 @@ from .views import CareersView
 urlpatterns = [
     path('careers/', CareersView.as_view(), name='careers'),                           # GET, POST (careers)
     path('careers/<int:id>/', CareersView.as_view(), name='careers-detail'),           # PATCH, DELETE (careers)
-
-    
 ]
--- project/urls.py
+++ project/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', include('homepage.urls')),
     path('api/', include('careers.urls')),
     path('api/auth/', include('authenticate.urls')),
 ]
